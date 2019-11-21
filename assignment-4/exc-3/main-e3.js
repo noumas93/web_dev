15ac2f2fc8fefec526442df0c473abcaccacb43a
mohamad masri <mohma171@student.liu.se>
2019-11-21 11:28:58 +0100

    task-4 completed

--- assignment-4/exc-3/main-e3.js
+++ assignment-4/exc-3/main-e3.js
@@ -1,6 +1,7 @@
 $("document").ready(function () {
-  console.log("kaka");
 
+
+  $("#myTable").tablesorter();
   $(".draggable").draggable({
     helper: "clone",
     revert: true,
@@ -10,30 +11,76 @@ $("document").ready(function () {
   $(".img-box").droppable({
     accept: "img",
     drop: function (event, ui) {
-      // ui är objektet "bilden" som dras
-      console.log(ui.draggable.attr("src"));
-      //event.target är div som objektet släpps i
-      console.log(event.target);
-      console.log("kaka");
-      let backImg = ui.draggable.attr('src');
-      console.log(backImg);
-      $(this).css("background-image", "url(" + backImg + ")");
-      $(this).css("pointer-events", "auto")
+      var backImg = ui.draggable.attr('src');
 
+      var activeImg = $(this).css("background-image", "url(" + backImg + ")");
+
+      console.log("ui " +backImg);
+      compareImg(backImg);
+      // ui är objektet "bilden" som dras
+      
+      //event.target är div som objektet släpps i
+    
+      //let backImg = ui.draggable.attr('src');
+      $(this).css("background-image", "url(" + backImg + ")");
+      $("img", this).attr("src", backImg);
+      $(this).css("pointer-events", "auto");
     }
   });
   $(".img-box").click(function () {
+
+    // $(".img-box").css("height", "500")
+    // .css("width", "500")
     console.log($(this).css("background-image"));
     console.log("innanför if");
 
 
     console.log("innanför else");
     $(this).featherlight();
+  });
 
+  $(".resetButton").click(function () {
+    $(".img-box").each(function () {
+      $(this).css("background-image", "");
+      $("img", this).attr("src", "");
+      $(this).css("pointer-events", "none")
+    });
+  });
 });
-});
+
+/*
+1.skicka in alla bilder från img-box
+2. skicka in droppad bild
+3. jämföra alla bilder med droppad bild
+4. om vi hittar duplicera bild; return så att drop eventet stoppas
+5. om ingen duplikant hittas, gör ingenting.
+*/
 
 
-//
-//$(".img-box").droppable() {
-//});
+var compareImg = function (droppedImg) {
+  console.log("compare " + droppedImg);
+/*
+  $(".img-box").each(function () {
+    var imge = droppedImg;
+    console.log("image in the box " +  imge);
+
+    if (droppedImg != imge) {
+      console.log("inside if");
+      return;
+    }
+  })
+  */
+ var imge = 0;
+ $(".img-box").each(function () {
+
+  imge++ ;
+ })
+console.log(" ss"  + imge);
+for (var i = 0; i < imge; i++) {
+
+  var ss = $("#imgBox" + i, "img").attr('src');
+  console.log(" ss " +ss);
+}
+
+
+};
